53e04001b6529bd2a3a1b7f8bda20114cac4ce89
Sundar Karthik S <sundar1007karthik@gmail.com>
2025-03-30 17:10:25 +0530

    qstash bug

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -13,7 +13,7 @@ export const createSubscription = async (req, res, next) =>{
 
         const {workflowRunId } = await workFlowClient.trigger({
             url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
-            method: 'POST',
+
             body: {
                     subscriptionId: subscription.id,
             },
@@ -99,7 +99,7 @@ export const sendRemindersBySubscriptionId = async (req, res, next) => {
             try {
                 const { workflowRunId } = await workFlowClient.trigger({
                     url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
-                    method: 'POST',
+
                     body: {
                         subscriptionId: subscription.id,
                     },
